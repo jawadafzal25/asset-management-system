e1bfc1d0121a1b8911d7af5b90c65cd138092bba
Muhammad Jawad Afzal <84334139+jawadafzal25@users.noreply.github.com>
2026-05-08 13:26:26 +0500

    updated employee module

--- routes/employee.php
+++ routes/employee.php
@@ -4,13 +4,13 @@ use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('employees')->group(function () {
-    Route::get('/', [EmployeeController::class, 'read']);
-    Route::post('/', [EmployeeController::class, 'create']);
-
-    // Group routes that require ID validation
+    // Single route for both "Read All" and "Read Single"
+    // The middleware still runs to protect the ID if it's provided
+    Route::get('/read/{id?}', [EmployeeController::class, 'read'])->middleware('check.employee');
+    Route::post('/create', [EmployeeController::class, 'create']);
     Route::middleware('check.employee')->group(function () {
-        Route::get('/{id}', [EmployeeController::class, 'read']);
-        Route::put('/{id}', [EmployeeController::class, 'update']);
-        Route::delete('/{id}', [EmployeeController::class, 'delete']);
+        Route::put('/update/{id}', [EmployeeController::class, 'update']);
+        Route::delete('/delete/{id}', [EmployeeController::class, 'delete']);
     });
 });
+
